df34a6d8fbfaf3031060f394d477a8503d4f692a
Hannah Engelhardt <hannahnengel@gmail.com>
2022-01-05 16:47:11 -0800

    Data Filtering  Filter the data for Monday  Sort the times listed for Monday in order  Ensure filtered data is funneled in in chart and defaulting to Monday table/data when user hits clicks 'submit' on the new entry form  Ensure table is updated updated in real time after user clicks 'submit'

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ var $entryForm = document.querySelector('.entry-form');
 var $modal = document.querySelector('.modal');
 var $greyOverlay = document.querySelector('.grey-overlay');
 var $addEntryButton = document.querySelector('.add-entry-button');
+var $scheduledEventsRow = document.querySelector('.scheduled-events-row');
+var $scheduledEventsColumn = document.querySelector('.scheduled-events-column');
+var $weekButtons = document.querySelector('.week-buttons');
+var $tbody = document.querySelector('tbody');
 
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -16,6 +20,25 @@ $entryForm.addEventListener('submit', function (event) {
   data.push(objectOfValues);
   $entryForm.reset();
   hideModal();
+
+  var newEntry = renderEntries(objectOfValues);
+  var $tds = document.querySelectorAll('td');
+  // will need to add if statement for other days of the week //
+  var filteredData = [];
+  var allData = [];
+  for (var i = 0; i < $tds.length; i++) {
+    allData.push(parseInt($tds[i].innerHTML));
+  }
+  for (var j = 0; j < allData.length; j += 2) {
+    filteredData.push(parseInt(allData[j]));
+  }
+  var insertBeforeIndex;
+  for (var k = 0; k < filteredData.length; k++) {
+    if (parseInt(newEntry.firstChild.innerHTML) >= filteredData[k] && parseInt(newEntry.firstChild.innerHTML) <= filteredData[k + 1]) {
+      insertBeforeIndex = (k + 2);
+    }
+  }
+  $tbody.insertBefore(newEntry, $tbody.childNodes[insertBeforeIndex]);
 });
 
 $addEntryButton.addEventListener('click', function (event) {
@@ -32,15 +55,56 @@ function openModal() {
   $greyOverlay.classList.remove('hidden');
 }
 
-// function renderEntries(entry) {
+window.addEventListener('DOMContentLoaded', addAnEntry);
+function addAnEntry(entry) {
+  var entryDay;
+  if (data.length > 0) {
+    var dataArray = [];
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].day === 'Monday') {
+        dataArray.push(data[i]);
+      }
+    }
+    sortTimes(dataArray);
+    for (var l = 0; l < dataArray.length; l++) {
+      var newEntry = renderEntries(dataArray[l]);
+      $tbody.append(newEntry);
+      entryDay = dataArray[l].day;
+    }
+    var h2 = document.createElement('h2');
+    h2.textContent = 'Scheduled Events for' + ' ' + entryDay;
+    $scheduledEventsColumn.prepend(h2);
+    $weekButtons.classList.remove('invisible');
+    $scheduledEventsRow.classList.remove('hidden');
+  }
 
-var tr = document.createElement('tr');
-var tdTime = document.createElement('td');
+  if (data.length === 0) {
+    $weekButtons.classList.add('invisible');
+    $scheduledEventsRow.classList.add('hidden');
+  }
+}
 
-var tdDescription = document.createElement('td');
+function sortTimes(entry) {
+  for (var j = 1; j < entry.length; j++) {
+    for (var k = 0; k < j; k++) {
+      if (entry[j].time < entry[k].time) {
+        var x = entry[j];
+        entry[j] = entry[k];
+        entry[k] = x;
+      }
+    }
+  }
+}
 
-tr.appendChild(tdTime);
-tr.appendChild(tdDescription);
+function renderEntries(entry) {
 
-//   return tr;
-// }
+  var tr = document.createElement('tr');
+  var tdTime = document.createElement('td');
+  tdTime.textContent = entry.time;
+  var tdDescription = document.createElement('td');
+  tdDescription.textContent = entry.description;
+  tr.appendChild(tdTime);
+  tr.appendChild(tdDescription);
+
+  return tr;
+}
